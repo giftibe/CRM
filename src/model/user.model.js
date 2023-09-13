f35b0bfaeaef65d79c54ea38f3cafed0ff64fe92
Akolite <nnorompreciousakachukwu@gmail.com>
2023-09-13 14:52:43 +0100

    feat: handled the email verification

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -4,40 +4,39 @@ const rounds = parseInt(process.env.ROUNDS);
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-            lowercase: true,
-        },
-
-        password: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
     },
-    { timestamps: true }
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 userSchema.pre("save", async function (next) {
-    if (this.isModified("password") || this.isNew("password")) {
-        const salt = await bcrypt.genSalt(rounds);
-        this.password = await bcrypt.hash(this.password, salt);
-    }
-    next();
+  if (this.isModified("password") || this.isNew("password")) {
+    const salt = await bcrypt.genSalt(rounds);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
 });
 
 userSchema.pre("findOneAndUpdate", async function (next) {
-    const update = this.getUpdate();
-    if (update.password) {
-        const salt = await bcrypt.genSalt(rounds);
-        update.password = await bcrypt.hash(update.password, salt);
-    }
-    next();
+  const update = this.getUpdate();
+  if (update.password) {
+    const salt = await bcrypt.genSalt(rounds);
+    update.password = await bcrypt.hash(update.password, salt);
+  }
+  next();
 });
 const user = mongoose.model("user", userSchema);
 module.exports = user;
