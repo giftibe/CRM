3ca5edb98a542424925caddf6da3939f659d4a5b
Gift <Giftibe62@gmail.com>
2023-09-16 15:41:43 +0100

    feat/ added validations and password template

--- src/route/user.route.js
+++ src/route/user.route.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const userRouter = Router();
 const storage = require('../utils/multer')
-
+const {
+  validate_Account_Creation_Inputs,
+  validate_User_Login_Inputs,
+  validate_User_Update
+} = (validate = require('../middleware/joi.validations'))
 
 
 const {
@@ -19,8 +23,8 @@ const {
   uploadPhoto
 } = (userController = require("../controller/user.controller"));
 
-userRouter.post("/user/register", signUp);
-userRouter.post("/user/login", loginUser);
+userRouter.post("/user/register", validate_Account_Creation_Inputs, signUp);
+userRouter.post("/user/login", validate_User_Login_Inputs, loginUser);
 userRouter.post("/user/reset-password", forgotPassword);
 userRouter.post('/user/picture/:id', storage.single('file'), uploadPhoto)
 userRouter.post("/user/logout", loggedOut);
@@ -28,7 +32,7 @@ userRouter.get("/user", fetchAllUsers);
 userRouter.get("/user/reset-password/:id/:token", forgottenPassword);
 userRouter.get("/user/:id", findAUser);
 userRouter.patch("/user/setpassword", updatePassword);
-userRouter.patch("/user/:id", updateAUser);
+userRouter.patch("/user/:id", validate_User_Update, updateAUser);
 userRouter.delete("/user/:id", removeUser);
 userRouter.get("/user/verify-email/:token", verifyEmail);
 
